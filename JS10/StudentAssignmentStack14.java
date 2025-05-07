e23dc4340e62d3e5484f85e680aa376282e62576
GarinoWIjaya <bossgarino123@gmail.com>
2025-05-07 19:49:18 +0700

    Create StudentAssignmentStack14.java

---
+++ JS10/StudentAssignmentStack14.java
@@ -0,0 +1,89 @@
+public class StudentAssignmentStack14 {
+    Student14[] stack;
+    int top, size;
+
+    StudentAssignmentStack14(int size) {
+        this.size = size;
+        top = -1;
+        stack = new Student14[size];
+    }
+
+    boolean isFull() {
+        if (top==size-1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void push(Student14 std) {
+        if(!isFull()) {
+            top++;
+            stack[top] =std;
+        } else {
+            System.out.println("Stack is already full!");
+        }
+    }
+
+    Student14 pop() {
+        if(!isEmpty()) {
+            Student14 std = stack[top];
+            top--;
+            return std;
+        } else {
+            System.out.println("There is no data in Stack!");
+            return null;
+        }
+    }
+
+    Student14 peek() {
+        if (!isEmpty()) {
+            return stack[top];
+        } else { 
+            System.out.println("There is no data in Stack!");
+            return null;
+        }
+    }
+
+    Student14 bottom() {
+        if (!isEmpty()) {
+            return stack[0];
+        } else {
+            System.out.println("There is no data in Stack!");
+            return null;
+        }
+    }
+
+    int countAssignment() {
+        return top +1;
+    }
+
+    String convertToBinary(int grade) {
+        ConversionStack stack = new ConversionStack();
+        while (grade > 0) {
+            int mod = grade % 2;
+            stack.push(mod);
+            grade = grade / 2;
+        }
+        String binary = "";
+        while (!stack.isEmpty()) {
+            binary += stack.pop();
+        }
+        return binary;
+    }
+
+    void print() {
+        for (int i = 0; i <= top; i++) {
+            System.out.println(stack[i].nim + "\t" + stack[i].name +"\t" + stack[i].className);
+        }
+        System.out.println("");
+    }
+}
